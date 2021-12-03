9b961f270d04c8eb8ad8ecb88d2b27db8e6ad029
BoulmanHamza <hamzaboulman132@gmail.com>
2021-12-03 01:20:56 +0000

    Simple Projet PWA

---
+++ index2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1> Hello 2</h1>
+
+ <input type="file" id="capture" accept=".jpg" name="im" />choisir file
+
+
+ 
+</body>
+</html>
+
+
+
+<?php
+
+
+
+
+?>
